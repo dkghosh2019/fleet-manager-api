c83743792ae2fa7984ade5ba92d3fb45f95a2113
Dipak Ghosh <durja1008@gmail.com>
2026-02-22 05:17:43 -0500

    move test profile under test/resources dir

--- src/test/java/com/lancy/fleetmanager/FleetManagerApplicationTests.java
+++ src/test/java/com/lancy/fleetmanager/FleetManagerApplicationTests.java
@@ -1,12 +1,11 @@
 package com.lancy.fleetmanager;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ActiveProfiles; // <--- ADD THIS
 
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles("test") // <--- ADD THIS to force H2
 class FleetManagerApplicationTests {
 
 	@Test
